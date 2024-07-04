aff73c0cd0c817927ee34442a590c118faa8e71d
Lutin <tclutin01@gmail.com>
2024-07-04 20:34:52 +0500

    add: faqfinder

---
+++ faqfinder_service/config/config.py
@@ -0,0 +1,19 @@
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    http_host: str
+    http_port: int
+
+    api_url: str
+    api_auth_url: str
+    api_login: str
+    api_password: str
+
+    jwt_secret_key: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
